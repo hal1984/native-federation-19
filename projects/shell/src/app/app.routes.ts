a4e8283c46581a7b8c4408e952e6b78562967f14
Jorge Díaz Díaz <hal_1984@hotmail.com>
2024-12-23 14:27:59 +0100

    add initial project structure with federation setup and basic components

---
+++ projects/shell/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
+import { Routes } from '@angular/router';
+import { DummyComponent } from '../dummy/dummy.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => loadRemoteModule({
+      remoteName: 'mfe1',
+      exposedModule: './Component',
+      fallback: DummyComponent
+    })
+  }
+];
