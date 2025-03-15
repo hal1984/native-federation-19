c51c5f9b568f3b7256ec0868df96e75543acd720
Jorge Díaz Díaz <hal_1984@hotmail.com>
2025-03-15 13:48:41 +0100

    add BarComponent and update routing to include it with redirects

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,14 +1,28 @@
 import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
+import { BarComponent } from './components/bar/bar.component';
 
 export const routes: Routes = [
   {
     path: '',
+    redirectTo: 'mfe1',
+    pathMatch: 'full',
+  },
+  {
+    path: 'mfe1',
     loadChildren: () =>
       loadRemoteModule({
         remoteName: 'mfe1',
         exposedModule: './routes',
         fallback: { routes: [] },
       }).then((m) => m.routes),
+  },
+  {
+    path: 'bar',
+    component: BarComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'mfe1',
   },
 ];
